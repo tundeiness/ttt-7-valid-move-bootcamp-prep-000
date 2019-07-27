bc7a440ab48cb265fdd70f31f32631fa5431dc65
tunde Oretade <tundeiness@yahoo.co.uk>
2019-07-27 15:39:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken?(board, index) && index.between?(0, 8)
-    true
+    false || nil 
   else 
-    false || nil
+    true
   end 
 end 
 
